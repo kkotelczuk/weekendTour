a3f15dbce527dc5f57b80f3fc75a2813c17d011f
Kamil Borecki <kamilborecki@MacBook-Air-Kamil.local>
2018-02-26 17:22:55 +0100

    last changes

--- src/containers/Map.js
+++ src/containers/Map.js
@@ -7,6 +7,7 @@ import LoadingIndicator from '../components/LoadingIndicator';
 import '../style/Map.css';
 
 class Map extends Component {
+  map = null;
   constructor(props) {
     super(props);
 
@@ -16,12 +17,16 @@ class Map extends Component {
       checkedMonuments: false,
       distance: 0,
       location: { lat: 52.2306, lng: 19.3643 },
-      isLoading: true
+      isLoading: true,
+      markers: []
     }
 
     this.updateCheck = this.updateCheck.bind(this);
     this.handleDistanceSlider = this.handleDistanceSlider.bind(this);
     this.getMap = this.getMap.bind(this);
+    this._clearMarkers = this._clearMarkers.bind(this);
+    this._loadPlaces = this._loadPlaces.bind(this);
+    this._placeMarkers = this._placeMarkers.bind(this);
   }
 
   componentDidMount() {
@@ -33,6 +38,7 @@ class Map extends Component {
        }}) 
       );
       this.getMap(12);
+      this._loadPlaces();
      }, () => {
       //if user won't agree to reveal location, we will show him full map of Poland
       //using lat and lng of central Poland hardcoded in the constructor     
@@ -48,11 +54,11 @@ class Map extends Component {
   getMap(zoom) {
     const { lat, lng } = this.state.location;
     
-    const map = new google.maps.Map(this.refs.map, {
+    this.map = new google.maps.Map(this.refs.map, {
       zoom,
       center: { lat, lng }
     });
-    google.maps.event.addListenerOnce(map, 'idle',() => {
+    google.maps.event.addListenerOnce(this.map, 'idle',() => {
       this.setState(() =>({isLoading: false}))
     });
   }
@@ -75,8 +81,84 @@ class Map extends Component {
       default:
         console.log("Hmm, that default case shouldn't be triggered. You messed up something :)");
     }
+    this._loadPlaces();
   }
+  _loadPlaces(){
+    this._clearMarkers();
+    if((!this.state.checkedAtractions && !this.state.checkedMonuments && !this.state.checkedViewpoints) || this.state.distance === 0){
+      return;
+    }
+    let queries = [];
 
+    this.state.checkedAtractions && queries.push('atractions');
+    this.state.checkedMonuments && queries.push('viewpoints'); 
+    this.state.checkedViewpoints && queries.push('monuments');
+    console.log(queries);
+    const location = new google.maps.LatLng(this.state.location.lat,this.state.location.lng);
+    const service = new google.maps.places.PlacesService(this.map);
+    const requests = queries.map(query=>{
+      return {
+        location: location,
+        radius: this.state.distance * 1000,
+        query: query
+      }
+    })
+    requests.map(request=>service.textSearch(request, this._placeMarkers))
+    
+  } 
+_clearMarkers(){
+  const markers = this.state.markers;
+  markers.map(marker=>{
+    if(marker){
+      marker.setMap(null);
+    }
+  });
+  this.setState({markers: []});
+}
+async _placeMarkers(data){
+  let markers = [];
+  console.log(data);
+  (data || []).map(async place=>{
+    let marker = new google.maps.Marker({
+      position: place.geometry.location,
+      map: this.map,
+      title: place.name
+    });
+    let photo = await this._loadPhoto(place);
+    let content = `
+    <div class="placeInfo">
+      <img src="${photo}"/>
+      <div class="info">
+      <p class="name">
+      ${place.name}
+      </p>
+      <p class="address">
+      ${place.name}
+      </p>
+      </div>
+    </div>
+    `;
+    let infowindow = new google.maps.InfoWindow({
+      content
+    });
+    marker.addListener('click', function() {
+      infowindow.open(this.map, marker);
+    });
+    markers.push(marker);
+  })
+  const stateMarkers = this.state.markers;
+  this.setState({markers: [...stateMarkers, markers]});
+  
+}
+async _loadPhoto(place){
+  if(!place.photos){
+    return place.icon 
+  }else{
+    let url = '';
+    url = await place.photos[0].getUrl({maxHeight: 100});
+    return url;
+  }
+}
   render() {
     const sliderStyle = {
       width: '150px'
